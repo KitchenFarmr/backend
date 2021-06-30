02b7e514e8086c459bab4079805338cfe526b338
Starlight Romero <starlightromero@protonmail.com>
2021-06-29 18:27:51 -0700

    [create] app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
+app.listen(3000);
+
+module.exports = app;
